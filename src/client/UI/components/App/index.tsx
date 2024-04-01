5616338adfc5bf7cf67a68571e2bd974d5ce1638
Seth Miller <71085273+sm3136@users.noreply.github.com>
2024-04-01 11:56:05 -0400

    Added raytracing enabled button

--- src/client/UI/components/App/index.tsx
+++ src/client/UI/components/App/index.tsx
@@ -1,11 +1,13 @@
 import Roact from "@rbxts/roact";
 import { MusicSetter } from "../MusicSetter";
+import { RayTraceEnabled } from "../RaytraceEnabled";
 
 export const App: Roact.FunctionComponent = () =>
 {
 	return (
 		<screengui ResetOnSpawn={false}>
 			<MusicSetter />
+			<RayTraceEnabled />
 		</screengui>
 	);
 };
